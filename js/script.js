5e31ce0e8fc9e31c1f89818c4b740d03f792b797
andrey <mail.ex@.ru>
2022-02-21 03:45:57 +0300

    21_02_21_commit

---
+++ js/script.js
@@ -0,0 +1,77 @@
+"Use strict";
+
+console.log( 1 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+//incr++;
+//decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log(4*2 == "8");
+
+console.log(true&&!false);
+console.log(true||false);
+
+console.log( 2 + 2 * 2 != 6);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let name = prompt("как вас зовут?","");
+//alert(`Привет, ${name}`);
+//let result = confirm("are your here?");
+//console.log(result);
+//if (result==true) {
+//  result = 'da';
+//}
+//else {
+//  result = 'net';
+//}
+//
+//alert(result);
+//
+//let answer = prompt ("сколько вам лет?","введите возраст");
+//console.log(answer);
+//alert("вам " + answer + " лет");
+//console.log(typeof(answer)); 
+//alert(typeof(answer));
+
+
+////let answers = [];
+
+//answers[0] = prompt("Как ваше имя?","");
+//answers[1] = prompt("Как ваша фамилия?","");
+//answers[2] = prompt("Сколько вам лет?","");
+
+//console.log(answers);
+//document.write(answers);
+
+//let category = "toys";
+
+//console.log(`https://someurl.com/${category}/5`);
+
+//let user = "Андрей";
+
+//alert(`Привет, ${user}`);
